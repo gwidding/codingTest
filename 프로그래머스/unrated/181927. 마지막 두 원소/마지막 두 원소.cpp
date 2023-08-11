1f01478cf3cfdc8eb5a5954f2ba26cc9d93945f9
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-08-11 12:45:03 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    int n = num_list.size();
+    
+    if (num_list[n-2] < num_list[n-1]){
+        num_list.push_back(num_list[n-1] - num_list[n-2]);
+    }
+    else num_list.push_back(num_list[n-1] * 2);
+    return num_list;
+}
