5da7c478fdd51db0766d5a9ca19acbd896776e29
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-08-11 15:45:50 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.15 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+
+    for (int i = 0; i < queries.size(); i++){
+        int tmp = arr[queries[i][1]];
+        arr[queries[i][1]] = arr[queries[i][0]];
+        arr[queries[i][0]] = tmp;
+    }
+    return arr;
+}
