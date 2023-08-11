235b53fe516531730cdd02e9058844a0fe6237d3
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-08-11 15:45:19 +0900

    [unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string str1, string str2) {
+    string answer;
+    for (int i = 0; i < str1.size() * 2; i++)
+        answer += (i % 2 == 0) ? str1[i/2]:str2[i/2] ;
+    return answer;
+}
