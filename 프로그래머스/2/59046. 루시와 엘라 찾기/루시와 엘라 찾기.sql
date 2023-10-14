4528850d60bf3641a48458028f31b1774fb3852d
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-14 23:09:12 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+# SELECT animal_id, name, sex_upon_intake
+# FROM animal_ins
+# WHERE name in ('Lucy', 'Ella', 'Pickle', 'Rogan' 'Sabrina', 'Mitty')
+# ORDER BY animal_id;
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
