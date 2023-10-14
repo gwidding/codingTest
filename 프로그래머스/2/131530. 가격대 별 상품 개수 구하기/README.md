79cccf5164170aebf57d2a05f17e714866a3494b
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-14 23:55:19 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 가격대 별 상품 개수 구하기 - 131530 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 10월 6일 23:43:22
+2023년 10월 6일 23:55:27
 
 ### 문제 설명
 
--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,15 +1,6 @@
 -- 코드를 입력하세요
-SELECT case 
-            when price < 10000 then 0
-            when price < 20000 then 10000
-            when price < 30000 then 20000
-            when price < 40000 then 30000
-            when price < 50000 then 40000
-            when price < 60000 then 50000
-            when price < 70000 then 60000
-            when price < 80000 then 70000
-            when price < 90000 then 80000
-       end as price_group,
+SELECT case when price < 10000 then 0
+            else (price div 10000) * 10000 end as price_group,
        count(*) as products
 FROM product
 GROUP BY price_group
