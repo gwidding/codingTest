bf1fc47cef864659e77ae579f7288545a666d005
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-14 23:12:19 +0900

    Update 루시와 엘라 찾기.sql

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,10 +1,5 @@
 -- 코드를 입력하세요
-# SELECT animal_id, name, sex_upon_intake
-# FROM animal_ins
-# WHERE name in ('Lucy', 'Ella', 'Pickle', 'Rogan' 'Sabrina', 'Mitty')
-# ORDER BY animal_id;
-
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-ORDER BY ANIMAL_ID;
+SELECT animal_id, name, sex_upon_intake
+FROM animal_ins
+WHERE name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY animal_id;
