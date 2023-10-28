1ed2f9f025f80f79fc9c543c40f08e8405ee5399
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-10-29 00:55:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -6,8 +6,7 @@ using namespace std;
 string solution(int n) {
     string answer = "";
     for (int i = 1; i <= n; i++) {
-        if (i % 2 == 1) answer += "수";
-        else answer += "박";
+        answer += (i % 2 == 1) ? "수" : "박";
     }    
     
     return answer;
