4e6051fdb5c91395844088218b702e8921626f20
귀띵 <135992700+gwidding@users.noreply.github.com>
2023-11-19 23:25:06 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    int len = s.size();
+    if (len != 4 && len != 6) {
+        return false;
+    }
+    for (int i = 0; i < len; i++) {
+        if (!isdigit(s[i])) {
+            return false;
+        }
+    }
+    return true;
+}
