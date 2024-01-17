6c6d904441558a7883dea69b70d652cf9a5c0f67
귀띵 <135992700+gwidding@users.noreply.github.com>
2024-01-17 19:27:40 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940#qna) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.01 ms
+메모리: 3.67 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 16:53:17
+2024년 1월 3일 19:27:38
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -1,14 +1,38 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <numeric>
 
 using namespace std;
 
+int gcd (int a, int b) {
+    
+    while (b != 0) {
+        if (a % b == 0) {
+            return b;
+        }
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return b;
+}
 
+
+int lcm (int a, int b) {
+    int tmp = gcd(a, b);
+
+    return tmp * (a / tmp) * (b / tmp);
+}
 
 vector<int> solution(int n, int m) {
     vector<int> answer;
+    
+    if (m > n) {
+        int tmp = n;
+        n = m;
+        m = tmp;
+    }
+    cout << gcd(36, 24) << " " << lcm(36, 24);
 
     answer.push_back(gcd(n,m));
     answer.push_back(lcm(n,m));
