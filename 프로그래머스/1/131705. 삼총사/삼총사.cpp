cfadfd4d0996f05ca512a0ed99c6130faf11338e
귀띵 <135992700+gwidding@users.noreply.github.com>
2024-02-11 17:33:21 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int n = number.size();
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int sum = number[i] + number[j];
+            for (int k = j+ 1; k < n; k++) {
+                if (number[k] == -sum) {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
