5ad4d76c7df1a2635bc1f915d4ef2030bef5ec30
귀띵 <135992700+gwidding@users.noreply.github.com>
2024-02-14 21:39:30 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,15 +1,15 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int area = brown + yellow;
+    
     for (int i = brown / 2; i > 1 ; i--) {
         int k = (area % i == 0) ? area/i : 0;
-        cout << k << endl;
+
         if (i*k - brown == yellow && 2*(i+k-2) == brown) {
             answer.push_back(i);
             answer.push_back(k);
