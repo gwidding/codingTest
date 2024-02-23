1bc0afeb609d9c2ae757b055fba1770cfd307ead
귀띵 <135992700+gwidding@users.noreply.github.com>
2024-02-23 16:10:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = log(n) / log(2);
+    if (a > b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    
+    while (n > 0) {
+        n /= 2;
+        if (a <= n && n < b)
+            break;
+        else if (a > n) {
+            a -= n;
+            b -= n;
+        }
+            
+        answer--;
+    }
+    
+    return answer;
+}
