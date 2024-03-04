ba60301c0b5364c315e256b79ae8aed8110167a7
귀띵 <135992700+gwidding@users.noreply.github.com>
2024-03-04 15:40:45 +0900

    [level 2] Title: 귤 고르기, Time: 37.73 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -2,34 +2,53 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <map>
 
 using namespace std;
-
 int solution(int k, vector<int> tangerine) {
-    if (k == 1)
-        return 1;
     
     int answer = 0;
-    vector<int> cntVec;
-    sort(tangerine.begin(), tangerine.end());
+    map<int, int> cntMap;
     
-    for (int i = 0; i < tangerine.size(); i++) {
-        int cnt = 1;
-        while (tangerine[i] == tangerine[i+1]) {
-            cnt++;
-            i++;
-        }
-        cntVec.push_back(cnt);
+    for (auto& i : tangerine) {
+        cntMap[i]++;    
     }
-    sort(cntVec.rbegin(), cntVec.rend());    
-    
-    while (k > 0) {
-        k -= cntVec[answer];
+    vector<int> cntVec;
+    for (auto& c : cntMap) {
+        cntVec.push_back(c.second);
+    }
+    sort(cntVec.rbegin(), cntVec.rend());
+    for (int i = 0; i < cntVec.size() && k > 0; i++) {
+        k -= cntVec[i];
         answer++;
     }
-    for (auto& t : cntVec) {
-        cout << t << " ";
-    }
-    
+
     return answer;
 }
+
+// int solution(int k, vector<int> tangerine) {
+//     if (k == 1)
+//         return 1;
+    
+//     int answer = 0;
+//     vector<int> cntVec;
+//     sort(tangerine.begin(), tangerine.end());
+    
+//     for (int i = 0; i < tangerine.size(); i++) {
+//         int cnt = 1;
+//         cout << tangerine[i+1] << " ";
+//         while (tangerine[i] == tangerine[i+1]) {
+//             cnt++;
+//             i++;
+//         }
+//         cntVec.push_back(cnt);
+//     }
+//     sort(cntVec.rbegin(), cntVec.rend());    
+    
+//     while (k > 0) {
+//         k -= cntVec[answer];
+//         answer++;
+//     }
+    
+//     return answer;
+// }
