22352485546bb4cb0cdbee6d670a8d8311a03d21
귀띵 <135992700+gwidding@users.noreply.github.com>
2025-02-15 18:59:19 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <iostream>
 
 using namespace std;
 
@@ -12,16 +11,12 @@ int solution(string word) {
     char direc[5] = {'A', 'E', 'I', 'O', 'U'};
 
     for (int i = 0; i < len; i++) {
-        cout << "i = " << i << " ";
         for (int j = 0; j < 5; j++) {
             if (word[i] != direc[j]) {
                 answer += (pow(5, 5-i) - 1) / (5 - 1);
-                cout << answer << " " ;
             }
-            else {                
-                cout << endl;
-                break;
-            }            
+            else
+                break;         
         }
     }    
     
