091c4efc4a2c69ce5795156ad89cb79910d4c3ae
귀띵 <135992700+gwidding@users.noreply.github.com>
2025-06-07 18:37:38 +0900

    [level 2] Title: 올바른 괄호, Time: 6.83 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 56.2 MB, 시간: 7.32 ms
+메모리: 54.8 MB, 시간: 6.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 07일 18:34:40
+2025년 06월 07일 18:37:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,13 +3,13 @@ class Solution {
         int ready = 0;
         
         for (char c : s.toCharArray()) {
-            if (c == '(') {
+            if (c == '(')
                 ready++;
-            }
-            else if (c == ')' && (ready < 1))
-                return false;
             else if (c == ')')
                 ready--;
+            
+            if (ready < 0)
+                return false;
         }
         if (ready != 0)
             return false;
