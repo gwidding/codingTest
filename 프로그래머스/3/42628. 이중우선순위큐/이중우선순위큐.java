622c1dc6224491959f9c961c3a72e0e93eaff61a
귀띵 <135992700+gwidding@users.noreply.github.com>
2025-06-23 16:32:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 83.16 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,27 +4,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = {0, 0};
-        ArrayList<Integer> list = new ArrayList<>();
+
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
         
         for (String str : operations) {
             String[] oper = str.split(" ");
             
-            if (oper[0].equals("I"))
-                list.add(Integer.parseInt(oper[1]));
-            else if (oper[0].equals("D") && list.size() > 0) {
-                Collections.sort(list);
-                if (oper[1].equals("1")) {
-                    list.remove(list.size()-1);
-                }
-                else if (oper[1].equals("-1")) {
-                    list.remove(0);
-                }
+            if (oper[0].equals("I")) {
+                minQ.add(Integer.parseInt(oper[1]));
+                maxQ.add(Integer.parseInt(oper[1]));
+            }
+            else if (oper[0].equals("D") && minQ.size() > 0) {
+                if (oper[1].equals("1"))
+                    minQ.remove(maxQ.poll());
+                else if (oper[1].equals("-1"))
+                    maxQ.remove(minQ.poll());
             }
         }
-        if (list.size() > 0) {
-            Collections.sort(list);
-            answer[1] = list.get(0);
-            answer[0] = list.get(list.size()-1);
+        
+        if (minQ.size() > 0) {
+            answer[0] = maxQ.poll();
+            answer[1] = minQ.poll();
         }
         return answer;
     }
