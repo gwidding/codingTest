c7cd8023eb678d1253c4738ebcccd6ae726f53bf
귀띵 <135992700+gwidding@users.noreply.github.com>
2025-06-29 18:29:32 +0900

    [level 3] Title: N으로 표현, Time: 47.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -7,19 +7,21 @@ class Solution {
         for (int i = 1; i < 9; i++) {
             dp[i] = new HashSet<>();
             int nnn = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp[i].add(nnn);         
+            dp[i].add(nnn);
             
             for (int j = 1; j < i; j++) {
                 for (int a : dp[j]) {
                     for (int b : dp[i-j]) {
                         dp[i].add(a + b);
                         dp[i].add(a - b);
+                        dp[i].add(b - a);
                         dp[i].add(a * b);
                         if (b != 0) dp[i].add(a / b);
+                        if (a != 0) dp[i].add(b / a);
                     }
                 }
             }
-            
+            System.out.println(dp[i]);
             if (dp[i].contains(number)) return i;
         }
 
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
 
 ### 성능 요약
 
-메모리: 85.6 MB, 시간: 26.63 ms
+메모리: 76 MB, 시간: 47.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 29일 18:22:08
+2025년 06월 29일 18:29:46
 
 ### 문제 설명
 
